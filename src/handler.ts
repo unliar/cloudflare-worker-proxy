04ea729e5c402d5ac7eff2de089e4d608098a0b8
blessyou <370732889@qq.com>
2022-04-19 11:07:54 +0800

    feat: init

--- src/handler.ts
+++ src/handler.ts
@@ -1,22 +1,21 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const url = new URL(request.url)
-  // 目标地址
-  const target = url.searchParams.get('target')
-
   // 需要目标地址
-  if (!target) {
+
+  // 读取请求体
+  const r = (await request.json()) as any
+  const { url, body } = r
+  if (!url) {
     return new Response(
       JSON.stringify({
         code: 422,
-        message: 'Missing target',
+        message: 'Missing url',
       }),
       { status: 422, headers: { 'Content-Type': 'application/json' } },
     )
   }
-  // 读取请求体
-  const body = await request.json()
+
   // 发送请求
-  return fetch(target, {
+  return fetch(url, {
     method: request.method,
     headers: request.headers,
     body: request.method == 'POST' ? JSON.stringify(body) : null,
